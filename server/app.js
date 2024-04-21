c46a5d83b339ec55eced664dad895d82fbb6e34c
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2024-04-21 19:32:59 +0530

    folder structure upgrade

--- server/app.js
+++ server/app.js
@@ -3,5 +3,5 @@ const express=require("express")
 const app=express()
 
 
-
+app.use("/api/user",userRouter)
 module.exports=app
---
+++ server/model/userModel.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose")
+
+let userSchema=new mongoose.Schema({
+    userName:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{timestamps:true})
+
+const USERS=mongoose.model("users", userSchema)
+
+module.exports=USERS
